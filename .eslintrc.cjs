f740c0ab79e4ebad8012934b14130fa1601cbd92
JUHEEBAEK <bjh9287@naver.com>
2024-10-03 17:31:07 +0900

    Chore: eslint 설정 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,27 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'consistent-return': 'error', // 모든 함수에서 일관된 방식으로 값을 반환하도록 강제. 코드의 일관성을 유지, 예기치 않은 동작 방지.
+    eqeqeq: ['error', 'always'], // 엄격한 동등성 비교 (===와 !==)만을 허용. 암묵적인 타입 강제 변환으로 인한 오류 방지
+    'prefer-const': 'error', // 변경되지 않는 변수를 let 대신 const로 선언하도록 강제
+    // 사용되지 않는 변수를 방지하여 코드가 불필요하게 복잡해지는 것을 막습니다.
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+    'no-console': 'warn',
+    'no-implicit-globals': 'error', // 전역 변수를 암묵적으로 선언하지 않도록 방지. 코드의 의도를 명확히 하고 전역 스코프의 오염을 줄임.
+    'no-shadow': 'error', // 상위 스코프에 있는 변수와 같은 이름의 변수를 하위 스코프에서 재선언 방지. 이는 혼란을 방지하고 코드 가독성을 높임.
+    // 코드에서 ‘매직 넘버’를 방지하고, 의미 있는 변수나 상수로 치환하도록 권장
+    'no-magic-numbers': ['warn', { ignore: [0, 1], ignoreArrayIndexes: true }],
+    'no-var': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
     'import/order': [
       'error',
       {
@@ -44,36 +65,55 @@ module.exports = {
         ],
         pathGroups: [
           {
-            pattern: '@hooks/*',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: '@pages/*',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: '@components/*',
-            group: 'internal',
-            position: 'after',
-          },
-          {
             pattern: 'react*',
             group: 'external',
             position: 'before',
           },
           {
-            pattern: '@/redux/*',
-            group: 'external',
+            pattern: '@storybook/*',
+            group: 'internal',
             position: 'before',
+          },
+          {
+            pattern: '@/**/*',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: './**/*.styles',
+            group: 'sibling',
+            position: 'after',
+          },
+          {
+            pattern: './**.css',
+            group: 'sibling',
+            position: 'after',
           },
         ],
         pathGroupsExcludedImportTypes: ['react'],
-        'newlines-between': 'always',
+        'newlines-between': 'always-and-inside-groups',
         alphabetize: {
           order: 'asc',
           caseInsensitive: true,
+        },
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+      {
+        selector: 'typeAlias',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^T[A-Z]',
+          match: true,
         },
       },
     ],
